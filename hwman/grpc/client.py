864eee930c9c660bd9c25f6a6840b2f9bb84668c
marcosf2 <marcosf2@illinois.edu>
2025-05-27 21:49:09 -0500

    First working prototype of gRPC

--- hwman/grpc/client.py
+++ hwman/grpc/client.py
@@ -1,16 +1,17 @@
 
-
+import uuid
 import logging
 
 import grpc
-from protobufs_compiled import messages_pb2, messages_pb2_grpc
+from protobufs_compiled import jobs_pb2, jobs_pb2_grpc
 
 
 def run():
     print("Starting gRPC client")
     with grpc.insecure_channel("localhost:50051") as channel:
-        stub = messages_pb2_grpc.JobDispatchStub(channel)
-        response = stub.SubmitJob(messages_pb2.JobRequest(id=1, type="dummy"))
+        stub = jobs_pb2_grpc.JobDispatchStub(channel)
+        job = jobs_pb2.Job(type=jobs_pb2.JobType.JOB_TYPE_DUMMY)
+        response = stub.SubmitJob(job)
         print(f"Response received: {response.id}, type: {response.type}, result: {response.result}")
 
 if __name__ == "__main__":
