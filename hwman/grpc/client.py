3827edda56cc67bf656dfc61e11c8567e393c9db
marcosf2 <marcosf2@illinois.edu>
2025-06-20 10:43:48 -0500

    Fixed ruff and mypy issues

--- hwman/grpc/client.py
+++ hwman/grpc/client.py
@@ -1,9 +1,7 @@
-
-import uuid
 import logging
 
 import grpc
-from protobufs_compiled import jobs_pb2, jobs_pb2_grpc
+from protobufs_compiled import jobs_pb2, jobs_pb2_grpc  # type: ignore
 
 
 def run():
@@ -12,7 +10,10 @@ def run():
         stub = jobs_pb2_grpc.JobDispatchStub(channel)
         job = jobs_pb2.Job(type=jobs_pb2.JobType.JOB_TYPE_DUMMY)
         response = stub.SubmitJob(job)
-        print(f"Response received: {response.id}, type: {response.type}, result: {response.result}")
+        print(
+            f"Response received: {response.id}, type: {response.type}, result: {response.result}"
+        )
+
 
 if __name__ == "__main__":
     logging.basicConfig()
--- hwman/grpc/server.py
+++ hwman/grpc/server.py
@@ -1,19 +1,23 @@
 import uuid
 import logging
-from pathlib import Path
 
 import grpc
 from concurrent import futures
 
-from hwman.grpc.protobufs_compiled import jobs_pb2_grpc
-from hwman.grpc.protobufs_compiled.jobs_pb2 import JobType, JobStatus, Result, DataAxis, Job
-from hwman.grpc.protobufs_compiled.jobs_pb2_grpc import JobDispatchServicer
+from hwman.grpc.protobufs_compiled import jobs_pb2_grpc  # type: ignore
+from hwman.grpc.protobufs_compiled.jobs_pb2 import (  # type: ignore
+    JobType,
+    JobStatus,
+    Result,
+    DataAxis,
+    Job,
+)
+from hwman.grpc.protobufs_compiled.jobs_pb2_grpc import JobDispatchServicer  # type: ignore
 from hwman.setup_measurements import execute_measurement
 from hwman.measurements.dummy import generate_dummy_sweep
 
 
 def dummy_measurement():
-
     sweep = generate_dummy_sweep()
     data_location, data = execute_measurement(sweep, "dummy_measurement")
 
@@ -22,14 +26,17 @@ def dummy_measurement():
         axis = DataAxis(name=ax, values=list(int(x) for x in data[ax]["values"]))
         axes.append(axis)
     for dep in data.dependents():
-        axis = DataAxis(name=dep, values=list(int(x) for x in data[dep]["values"]), depends_on=data[dep]["axes"])
+        axis = DataAxis(
+            name=dep,
+            values=list(int(x) for x in data[dep]["values"]),
+            depends_on=data[dep]["axes"],
+        )
         axes.append(axis)
     result = Result(id=str(uuid.uuid4()), path=str(data_location), data=axes)
     return result
 
 
-class JobDispatchServicer(JobDispatchServicer):
-
+class JobService(JobDispatchServicer):
     def SubmitJob(self, request, context):
         # Process the job request and return a response
         print(f"Received job request: {request.id}, type: {request.type}")
@@ -37,12 +44,14 @@ class JobDispatchServicer(JobDispatchServicer):
         if request.type == JobType.JOB_TYPE_DUMMY:
             # Execute the dummy measurement
             result = dummy_measurement()
-            ret = Job(id=request.id,
-                      user=request.user,
-                      type=request.type,
-                      status=JobStatus.JOB_STATUS_SUCCESS,
-                      payload=request.payload,
-                      result=result,)
+            ret = Job(
+                id=request.id,
+                user=request.user,
+                type=request.type,
+                status=JobStatus.JOB_STATUS_SUCCESS,
+                payload=request.payload,
+                result=result,
+            )
 
         else:
             raise NotImplementedError(f"Job type {request.type} not implemented")
@@ -53,11 +62,12 @@ class JobDispatchServicer(JobDispatchServicer):
 def serve():
     port = "50051"
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    jobs_pb2_grpc.add_JobDispatchServicer_to_server(JobDispatchServicer(), server)
+    jobs_pb2_grpc.add_JobDispatchServicer_to_server(JobService(), server)
     server.add_insecure_port(f"[::]:{port}")
     server.start()
     print(f"Server started on port {port}")
     server.wait_for_termination()
+
 
 if __name__ == "__main__":
     logging.basicConfig()
